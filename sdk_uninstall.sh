39dfe26398b4b4372d90c8b6b74c2b9ed5586d34
root <root@ubuntu.(none)>
2015-07-06 10:51:54 +0800

    v0.1

---
+++ sdk_uninstall.sh
@@ -0,0 +1,8 @@
+rm -f /usr/lib/i386-linux-gnu/*Qt*AV*
+rm -f /usr/lib/i386-linux-gnu/libQtAV.so
+rm -rf /usr/include/qt4/QtAV
+rm -f /usr/share/qt4/mkspecs/features/av.prf
+rm -f /usr/lib/i386-linux-gnu/*Qt*AV*
+rm -f /usr/lib/i386-linux-gnu/libQtAVWidgets.so
+rm -rf /usr/include/qt4/QtAVWidgets
+rm -f /usr/share/qt4/mkspecs/features/avwidgets.prf
